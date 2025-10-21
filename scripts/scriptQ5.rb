c2b873ba5166b2532d1d2b9e07426a5a6b98e76f
SCHOUVER Doryan <schouver@iutnc-503-07.iutnc.site.univ-lorraine.fr>
2025-10-21 11:15:26 +0200

    ajout de la fonction 'sort' pour trié par pourcentage d'utilisation

--- scripts/scriptQ5.rb
+++ scripts/scriptQ5.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
-
-puts `df -h -x tmpfs -x devtmpfs --output=source,size,used,avail,pcent`
+# Affiche l'espace disque par partition, trié par ordre décroissant de la colonne Uti%
+puts "Sys. de fichiers Uti% Taille Utilisé Dispo"
+puts `df -h -x tmpfs -x devtmpfs --output=source,pcent,size,used,avail | tail -n +2 | sort -k2 -nr`
